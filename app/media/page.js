5c77102aa19ae329334c491a1a094a82406ab08e
Muhammad <mhdibnhameed@gmail.com>
2023-10-14 18:21:51 +0530

    resized image

--- app/media/page.js
+++ app/media/page.js
@@ -8,8 +8,8 @@ const page = () => {
   const [getImage, setGetImage] = useState([]);
   const imageListsRef = ref(storage, "posters/");
   const handleClick = () => {
-    document.querySelector('.card').classList.add('hover');
-  }
+    document.querySelector(".card").classList.add("hover");
+  };
 
   useEffect(() => {
     listAll(imageListsRef).then((response) => {
@@ -23,15 +23,25 @@ const page = () => {
   return (
     <div className="flex flex-col">
       {getImage.length !== 0 ? (
-        <><h1 className="text-3xl font-semibold text-center">Posters</h1><div className="px-2 flex flex-col-reverse gap-y-8 w-full items-center my-4">
-          {getImage.map((url) => (
-            <div onClick={handleClick} className="card flex items-center justify-centerst shadow-lg border-3">
-              <img className="w-full h-full object-cover" src={url} />
-            </div>
-          ))}
-        </div></>
-      ) : <h1>Posters will be displayed later</h1>}
-      
+        <>
+          <h1 className="text-3xl font-semibold text-center">Posters</h1>
+          <div className="px-5 grid grid-cols-1 md:grid-cols-3  gap-5 w-full items-center my-10">
+            {getImage.map((url) => (
+              // <div
+              //   onClick={handleClick}
+              //   className="card flex items-center justify-centerst  border-3"
+              // >
+                <img
+                  className="w-full shadow-md rounded-sm h-auto object-cover"
+                  src={url}
+                />
+              // </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <h1>Posters will be displayed later</h1>
+      )}
     </div>
   );
 };
