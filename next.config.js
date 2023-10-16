dc763976087137fa80a496c1550eef195d0a3b05
Muhammad <mhdibnhameed@gmail.com>
2023-10-16 19:31:05 +0530

    re writed mongodb

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponents: true,
-  },
-  rewrites: async () => [
-    {
-      source: '/api/:path*',
-      destination: '/api/:path*',
-      has: [
-        {
-          type: 'header',
-          key: 'Cache-Control',
-          value: 'no-cache',
-        },
-      ],
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'firebasestorage.googleapis.com'
+      }
+    ]
+  }
 }
 
 
