b44ef8358c608ab5860c572739affdd8b5fa0023
Muhammad <mhdibnhameed@gmail.com>
2023-10-16 09:21:42 +0530

    nothing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponents: true,
+  },
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: '/api/:path*',
+      has: [
+        {
+          type: 'header',
+          key: 'Cache-Control',
+          value: 'no-cache',
+        },
+      ],
+    },
+  ],
+}
 
 
 module.exports = nextConfig
