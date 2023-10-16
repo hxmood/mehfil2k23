ace231704c2013f5c074e678c08302e4b15bd204
Muhammad <mhdibnhameed@gmail.com>
2023-10-16 07:11:23 +0530

    fixing

--- utils/database.jsx
+++ utils/database.jsx
@@ -1,12 +1,22 @@
 import mongoose from "mongoose"
 
+let isConnected = false
 
 const connectMongoDB = async () => {
+
+    mongoose.set("strictQuery",true)
+    if(isConnected) {
+        console.log("mongodb is aldready connected");
+        return
+    }
+
     try {
         await mongoose.connect(process.env.MONGODB_URI,{
-            dbName: "suffa_mehfil"
+            dbName: "suffa_mehfil",
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
-
+        isConnected = true
         console.log("mongodb is connected");
     } catch (error) {
         console.log(error)
